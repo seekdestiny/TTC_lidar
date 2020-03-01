54d7d8d1a8fbd4cd091ba644465b92bdcf050f61
seekdestiny <jifeiqian@gmail.com>
2020-02-29 17:00:51 -0800

    complete exercise

--- src/compute_ttc_lidar.cpp
+++ src/compute_ttc_lidar.cpp
@@ -19,11 +19,17 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     double minXPrev = 1e9, minXCurr = 1e9;
     for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
     {
+        if (abs(it->y) <= laneWidth / 2.0) {
+          continue;
+        }
         minXPrev = minXPrev > it->x ? it->x : minXPrev;
     }
 
     for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
     {
+        if (abs(it->y) <= laneWidth / 2.0) {
+          continue;
+        }
         minXCurr = minXCurr > it->x ? it->x : minXCurr;
     }
 
